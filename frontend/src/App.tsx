208dae034c958bda898c7cb172c5297b2283136d
403ABHIJEET <abhijeet8745@gmail.com>
2025-02-02 11:34:52 +0530

    tailwind and shdcn added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
-import Form from './pages/Form'
-import Show from './pages/Show'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <Routes>
-        <Route path='/form' element={<Form />} ></Route>
-        <Route path='/show' element={<Show />} ></Route>
-      </Routes>
-    </div>
+    <div></div>
   )
 }
 
